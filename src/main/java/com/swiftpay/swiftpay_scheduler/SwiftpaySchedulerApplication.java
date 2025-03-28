382c3705ea31693defbb0afa2523df008600de42
bink2b <talkto@bobtail.pt>
2025-03-28 17:42:36 +0000

    Initial commit

---
+++ src/main/java/com/swiftpay/swiftpay_scheduler/SwiftpaySchedulerApplication.java
@@ -0,0 +1,13 @@
+package com.swiftpay.swiftpay_scheduler;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SwiftpaySchedulerApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SwiftpaySchedulerApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/swiftpay/swiftpay_scheduler/SwiftpaySchedulerApplicationTests.java
@@ -0,0 +1,13 @@
+package com.swiftpay.swiftpay_scheduler;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SwiftpaySchedulerApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
